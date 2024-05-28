69bf2c96459ae56a07fb137c16dedb253905b041
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-05-28 19:36:13 +0000

    Created config.php and added code to connect to a database.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', 's7n.h.filess.io');
+define('DB_USER', 'COSC4806_orangecat');
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_DATABASE', 'COSC4806_orangecat');
+define('DB_PORT', '3305');
+
+?>
