552ae7dc5c1edc32300fc885484e31c0a475d497
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-05-29 19:39:15 +0000

    Use $GLOBALS to know when the DB is down.

--- database.php
+++ database.php
@@ -2,13 +2,17 @@
 
 require_once ('config.php');
 
+// Create a global variable to track if the DB is down.  
+$GLOBALS['DB_DOWN'] = false;
+
 function db_connect() {
   try {
     $dbh = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
     return $dbh;
   } catch (PDOException $e) {
-    // We should set a global variable here so we know the DB is down.
     echo "Database is down. Error: " . $e->getMessage();
+    // We should set a global variable here so we know the DB is down.
+    $GLOBALS['DB_DOWN'] = true;
     exit;
   }
 }
