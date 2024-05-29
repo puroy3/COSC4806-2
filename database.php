e55c21dbd8aad45f0ec87e0f84d65d642a570338
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-05-29 19:43:14 +0000

    Use a session variable instead of globals to know when the DB is down.

--- database.php
+++ database.php
@@ -1,9 +1,12 @@
 <?php
 
 require_once ('config.php');
+// Use session to track if the DB is down.
+session_start();
 
-// Create a global variable to track if the DB is down.  
+/* Create a global variable to track if the DB is down.  
 $GLOBALS['DB_DOWN'] = false;
+*/
 
 function db_connect() {
   try {
@@ -12,7 +15,8 @@ function db_connect() {
   } catch (PDOException $e) {
     echo "Database is down. Error: " . $e->getMessage();
     // We should set a global variable here so we know the DB is down.
-    $GLOBALS['DB_DOWN'] = true;
+    // $GLOBALS['DB_DOWN'] = true;
+    $_SESSION['DB_DOWN'] = true;
     exit;
   }
 }
