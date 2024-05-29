234d084c1136fc21401ac913c14b9964142d94a3
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-05-29 19:55:00 +0000

    Check the database status in index.php, login.php, signup.php, and user.php.

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 session_start();
 
+require_once('database.php');
+
+// Connect to database.
+$db = db_connect();
+
+// Check if the database is down.
+if (isset($_SESSION['DB_DOWN'])) {
+  echo "The database is down.";
+  exit;
+}
+
 // Check if user is authenticated.
 // If not, send them to login.php using header()...
 /*if (!isset($_SESSION['authenticated'])) {
--- user.php
+++ user.php
@@ -1,6 +1,16 @@
 <?php
+session_start();
 
-require_once ('database.php');
+require_once('database.php');
+
+// Connect to database.
+$db = db_connect();
+
+// Check if the database is down.
+if (isset($_SESSION['DB_DOWN'])) {
+  echo "The database is down.";
+  exit;
+}
 
 Class User {
 
