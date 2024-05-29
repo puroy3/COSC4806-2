44d70f63e3c00118cde62b7984db095f3aa02c18
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-05-29 20:08:39 +0000

    Check if the username already exists by comparing all usernames in database to the inputted username.

--- user.php
+++ user.php
@@ -22,8 +22,16 @@ Class User {
     return $rows;
   }
 
-  public function create_user ($username, $password) {
+  public function create_user($username, $password) {
     $db = db_connect();
+    // Check to see if the account username already exists.
+    $statement = $db->prepare("SELECT * FROM users WHERE username = ?");
+    $statement->execute([$username]);
+    // If the username already exists, return false.
+    if ($statement->fetchAll()) {
+      return false;
+    }
+    
     $statement = $db->prepare("INSERT into users //add stuff");
     $statement->execute();
     $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
