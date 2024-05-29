f9d921e9ff26c81ca987fcc68f7017d6e33c5618
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-05-29 20:56:36 +0000

    Create a new user. If the user is created successfully, redirect to login.php to sign in to the website. Otherwise, print a message telling the user that the username already exists.

--- signup.php
+++ signup.php
@@ -23,6 +23,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (strlen($password) < 11 || $password !== $password2) {
     echo "The password does not meet the minimum length requirement of 11 characters or the passwords do not match."
   }
+  // Otherwise, create a new user.  
+  else {
+    $user = new User();
+    // If the user is created successfully, redirect to login.php to sign in to the website.
+    if ($user->create_user($username, $password)) {
+      header("Location: login.php");
+      exit;
+    }
+    // Otherwise, print a message telling the user that the username already exists.
+    else {
+      echo "The username already exists.";
+    }
+  }
 }
 // Ask for 3 things:
 // Username
@@ -31,3 +44,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // Press "Create", check that the two passwords are the same, check that the username is not already in the table, and have a minimum security standard of at least eleven characters for the password. 
 // Redirect to login.php to sign in to the website.
 ?>
+
