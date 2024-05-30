7cc9aa6046bb64e00fc642d76f2fa23ffd160b18
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-05-30 18:18:48 +0000

    Updated validate.php to verify password.

--- validate.php
+++ validate.php
@@ -1,6 +1,14 @@
 <?php
   session_start();
   require_once('database.php');
+  // When a user signs in, we are going to take their username and password, hash the password, compare that hash to what's already in the database, and if they match, the user can log in to the system.
+//require_once('user.php');
+// Create user object.
+//$user = new User();
+// Check if data is requested.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
   // Connect to database.
   $db = db_connect();
   // Check if the database is down.
@@ -8,14 +16,6 @@
     echo "The database is down.";
     exit;
   }
-  // When a user signs in, we are going to take their username and password, hash the password, compare that hash to what's already in the database, and if they match, the user can log in to the system.
-require_once('user.php');
-// Create user object.
-$user = new User();
-// Check if data is requested.
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $username = $_POST['username'];
-  $password = $_POST['password'];
     // Don't hardcode username and password.
     /*$valid_username = "pushpak";
     $valid_password = "1";
@@ -30,14 +30,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     */
     // Authenticate the user.
-    $authenticate = $user->authenticate_user($username, $password);
+    //$authenticate = $user->authenticate_user($username, $password);
   
     // If the user is authenticated, redirect to index.php.
-    if ($authenticate) {
+    /*if ($authenticate) {
       $_SESSION['authenticated'] = true;
       $_SESSION['username'] = $username;
       header ('location: /');
       exit;
+    }
+    */
+    $statement = $db->prepare("select password FROM users WHERE username = :username");
+      $statement->execute([$username]);
+      $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+      return $rows;
+      
+    if ($rows && password_verify($password, $rows['password'])) {
+      $_SESSION['authenticated'] = true;
+      $_SESSION['username'] = $username;
+      header ('location: /');
     }
     else {
       if (!isset($_SESSION['failed_attempts'])){
@@ -46,7 +57,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       else {
         $_SESSION['failed_attempts'] = $_SESSION['failed_attempts'] + 1;
       }
-
       // header... redirect to login.php
       header("Location: login.php");
       exit;
